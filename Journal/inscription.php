93969436be3e07a7eaeb9dd5d80715f03df593d7
Kevin Quach <kevin.quach@outlook.fr>
2017-12-11 09:15:49 +0100

    ajout journal

---
+++ Journal/inscription.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <title>Contact</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles/style.css" type="text/css">
+  </head>
+
+  <body>
+    <!--Header et menu-->
+    <div class="part une">
+    <header id="header">
+      <div id="logo">
+        <h1><a href="./index.html">Logo Journal </a></h1>
+        <h2>Ecole Ipssi</h2>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Inscription</a></li>
+          <li><a href="#">Connexion</a></li>
+          <li class="last"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    </div>
+    <div class="part principal">
+      <div id="container" class="clear">
+          <div id="sub_title">
+              <h1> Journal Ipssi</h1>
+            </div>
+            <!-- Formulaire -->
+            <form id="form">
+                  <input id="email" type="text" placeholder="E-MAIL">
+                  <input id="name" type="text" placeholder="NOM">
+                  <input id="prenom" type="text" placeholder="PRENOM">
+                  <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+              <input id="submit" type="submit" value="GO!">
+              
+            </form>
+      </div>
+    </div>
+    <!-- footer -->
+    <div class="part foot">
+      <footer id="footer" class="clear">
+        <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Ecole-Ipssi</a></p>
+        <p class="fl_right"><a href="#" title="Free Website Templates">Kevin Quach</a></p>
+      </footer>
+    </div>
+  </body>
+</html>
